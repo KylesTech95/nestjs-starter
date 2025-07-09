837462d47c35c807667ef89d3a51d9024e8b7a7b
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2025-07-09 12:42:56 -0400

    Initial commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get()
+  addSum(a:number,b:number): number {
+    return this.appService.addSum(a,b);
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  // say hello
   getHello(): string {
     return 'Hello World!';
+  }
+  // add sum
+  addSum(a:number,b:number): number {
+    return a + b;
   }
 }
