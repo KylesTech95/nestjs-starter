7041bf4903b540dc9c0777d52e02d050836313eb
Primary User <primaryuser@Primarys-MacBook-Air.local>
2025-07-11 09:43:24 -0400

    chore: best practice: create constants file to hold unique providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as CON from './constants'
+const constants = JSON.stringify(CON.constants);
+console.log("Constants\n"+constants)
 
 // mock service
 const mockAppService = {
---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+// best practice to hold constant variables (provide property)
+// Treat them much as you would symbols or enums that are defined in their own file and imported where needed.
+export const constants =  {
+    connection: 'CONNECTION',
+    test: 'TEST',
+}
