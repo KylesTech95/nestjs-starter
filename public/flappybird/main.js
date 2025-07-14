96f4132daa84d6718f41b1d6c8d0fc12a40368db
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2025-07-14 13:26:07 -0400

    chore: update main.js with game data

--- public/flappybird/main.js
+++ public/flappybird/main.js
@@ -1,7 +1,8 @@
-// vars
-const h1 = document.querySelector('h1')
+// fetch canvas
 const canvasInit = '/flappy/canvas-init'
-const canvas = fetch(canvasInit, {method:'GET',header:{'Content-Type':'text/html'}}).then(r=>{
+if(!document.querySelector('canvas')){
+    console.log('canvas does not exist')
+    const canvas = fetch(canvasInit, {method:'GET',header:{'Content-Type':'text/html'}}).then(r=>{
     if(r.status===200){
         // console.log(r)
         return r.text();
@@ -9,17 +10,50 @@ const canvas = fetch(canvasInit, {method:'GET',header:{'Content-Type':'text/html
         console.log('status is bad')
     }
 }).then(canva => renderCanvas(canva)); // fetch canvas
+} else {
+    console.log('canvas exists')
+}
 /*-----------------------------------------------*/
-
-
-
 // render canvas
 function renderCanvas(canvas){
     let section = document.createElement('section');
     section.innerHTML = canvas;
-    // document.body.insertBefore(section,h1);
     document.body.append(section);
-    const seconds = 1*.5;
-    const vas = [...section.children][0];
+
+    let boardWidth = canvas.width;
+    let boardHeight = canvas.height;
+
+    let GAME_STATE = {
+        MENU:'menu',
+        PLAYYING:'playing',
+        GAME_OVER: 'gameOver'
+    }
+    let currentState = GAME_STATE.MENU;
+
+    let FLAG = {
+        inputLocked:false,
+    }
+
+    document.onkeydown = handleKeydown
+
+    let playBtn = {
+        x: boardWidth / 2 - (120/2),
+        y: boardHeight / 2 - (70 /2),
+        height: 120,
+        width:70
+    }
+
+    let logo = {
+        x: boardWidth / 2 - 300 / 2,
+        y: boardHeight / 4,
+        width:300,
+        height:100
+    }
+    let flappyBirdTextImg = new Img();
+    flappyBirdTextImg.src = ""
 }
 
+
+function handleKeydown(e){
+
+}
